03f3497308cd29b095bbc63c64cfcd49153ccb9e
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-19 17:23:00 +0530

    added

--- app/Header.jsx
+++ app/Header.jsx
@@ -3,10 +3,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/About"}>About</Link>
-    </div>
+    <ul
+      style={{
+        display: "flex",
+        gap: "20px",
+        listStyle: "none",
+        justifyContent: "center",
+      }}
+    >
+      <li>
+        <Link style={{ textDecoration: "none" }} href={"/"}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link style={{ textDecoration: "none" }} href={"/About"}>
+          About
+        </Link>
+      </li>
+    </ul>
   );
 };
 
