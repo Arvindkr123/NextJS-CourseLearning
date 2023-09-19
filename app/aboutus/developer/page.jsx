f968924c82e46f6b42abc96c15e07abf558561e7
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-19 16:22:10 +0530

    added nested routing in next js

---
+++ app/aboutus/developer/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div
+      style={{
+        margin: "auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize:"20px"
+      }}
+    >
+      I am frontend Developer Arvind Kumar..
+    </div>
+  );
+};
+
+export default page;
